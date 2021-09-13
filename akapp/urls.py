156d28fae6fd894f09333aef3072b19a36dc93ee
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-09-13 17:20:00 +0900

    v4-5

--- akapp/urls.py
+++ akapp/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
     path('anken/create/', views.AnkenCreateView.as_view(), name='anken_create'),
     path('anken/create/complete/', views.AnkenCreateCompleteView.as_view(), name='anken_create_complete'),
+    path('anken/list/', views.AnkenListView.as_view(), name='anken_list'),
+    path('anken/detail/<int:pk>/', views.AnkenDetailView.as_view(), name='anken_detail'),
+    path('anken/update/<int:pk>/', views.AnkenUpdateView.as_view(), name='anken_update'),
+    path('anken/delete/<int:pk>/', views.AnkenDeleteView.as_view(), name='anken_delete'),
 ]
--- akapp/views.py
+++ akapp/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import AnkenForm
 from django.urls import reverse_lazy
+from .models import Anken
+from django.utils import timezone
 
 class IndexView(TemplateView):
     template_name = 'akapp/index.html'
@@ -14,3 +16,45 @@ class AnkenCreateView(CreateView):
 
 class AnkenCreateCompleteView(TemplateView):
     template_name = 'akapp/anken_create_complete.html'
+
+
+class AnkenListView(ListView):
+    template_name = 'akapp/anken_list.html'
+    model = Anken
+
+
+class AnkenDetailView(DetailView):
+    template_name = 'akapp/anken_detail.html'
+    model = Anken
+
+
+class AnkenUpdateView(UpdateView):
+    template_name = 'akapp/anken_update.html'
+    model = Anken
+    fields = ('pub_date',
+                'ankenmei',
+                'iraibusho',
+                'iraisha',
+                'nouki',
+                'mitumorikousu',
+                'naiyou',
+                'genjouchi', 
+                'kitaikouka', 
+                'tantousha',  
+                'koumoku', 
+                'joutai', 
+                'jissekikousu',
+                )
+    success_url = reverse_lazy('akapp:anken_list')
+
+    def form_valid(self, form):
+        anken = form.save(commit=False)
+        anken.pub_date = timezone.now()
+        anken.save()
+        return super().form_valid(form)
+
+
+class AnkenDeleteView(DeleteView):
+    template_name = 'akapp/anken_delete.html'
+    model = Anken
+    success_url = reverse_lazy('akapp:anken_list')
