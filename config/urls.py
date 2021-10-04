691e49565612ea26de5f15f1cc1d53e306d21617
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-10-04 16:57:58 +0900

    ver3_3_4

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,16 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from config import settings
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('akapp.urls')),
     path('account/', include('allauth.urls')),
     path('tinymce/', include('tinymce.urls')),
     path('summernote/',include('django_summernote.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ]
-#if settings.DEBUG:
-#    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
